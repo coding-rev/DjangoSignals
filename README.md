36badc7e71313ab98e49cb5cc1d439e3cd57a3ea
codingrev <emmowu10@gmail.com>
2022-03-07 01:26:37 +0000

    README revised

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 DjangoSignals Under Study
 
 # Content
-## Post_Save Function
+## Post_Save Signal Function
 ### post_save signal function is sent at the end of the save() method.
 1. Car signal that automatically creates a car model for new user
+
+## Pre_Save  Signal Function
+### This is sent at the beginning of a model’s save() method.
 2. HumanID signal that adds an ID to created Human automatically
 
 ## Pre_delete Function
@@ -13,4 +16,4 @@ DjangoSignals Under Study
 
 # Signal Creation Flow
 1. Design the signal function
-2. Register the signal file to the app
+2. Register the signal file to the apps.py
--- lesson/signals/humanID_signal.py
+++ lesson/signals/humanID_signal.py
@@ -1,17 +1,16 @@
 # Signal import(s)
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 # Local Imports
 from lesson.models.human_model import Human
 from lesson.utils import generate_humanID
 
 # Signal Functions
-@receiver(post_save, sender=Human)
+@receiver(pre_save, sender=Human)
 def addIDToHuman(sender, **kwargs):
-    if kwargs['created']:
-        getHuman        = kwargs['instance']
-        getHuman.humanID= generate_humanID()
-        getHuman.save()
+    getHuman        = kwargs['instance']
+    getHuman.humanID= generate_humanID()
+   
 
         
 
